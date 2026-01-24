ad3681cbd8f75ce43b7efedd11cc3d0d211e62b6
Mahbod <mahbodtajdini@gmail.com>
2026-01-24 11:06:20 +0100

    added speedinsights

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <Router>
     <App />
+    <SpeedInsights />
   </Router>
 );
 
