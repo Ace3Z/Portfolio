a6b195e7797c33c8eb6367b0489e17981e4deea5
Mahbod <mahbodtajdini@gmail.com>
2026-01-24 11:13:00 +0100

    added vercel stuff

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
