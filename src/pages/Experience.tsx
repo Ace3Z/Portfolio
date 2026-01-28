7b7b82614246a9e2d21157df30276fa30e0d4723
Mahbod <mahbodtajdini@gmail.com>
2026-01-28 20:09:04 +0100

    url updates

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -20,6 +20,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'work',
     name: 'Forward·Inc',
     title: 'Student Consultant 🤵🏽‍♂️',
+    link: 'https://www.newcomersforward.com/get-involved/',
     summaryPoints: [
       '📁 Providing research, analysis, and practical recommendations for a mission-driven organization. Review programs, analyze data, and support strategic planning efforts. Collaborate with team members to improve operations, strengthen services, and support organizational goals.'
     ],
@@ -29,6 +30,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'teaching',
     name: 'ETH Zürich D-USYS',
     title: 'Teaching Assistant 🧑🏽‍🏫',
+    link: 'https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?semkez=2026S\&ansicht=ALLE\&lerneinheitId=201478\&lang=en',
     summaryPoints: [
       '📚 Serving as a Teaching Assistant for Environmental Systems Data Science: Machine Learning (701-3003-00L) under the supervision of Prof. Dr. Loïc Pellissier and Dr. Camille Pierre Albouy.'
     ],
@@ -38,6 +40,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'work',
     name: 'Analytics Club at ETH Zürich',
     title: 'Hack4Good Organizing Staff 🖥️',
+    link: 'https://www.analytics-club.org/hack4good',
     summaryPoints: [
       'Supporting the operations of Hack4Good, an 8-week pro-bono program connecting ETH Zürich students with non-profit organizations to develop data-driven solutions for social causes.',
       'Assisting in facilitating partnerships with non-profit organizations to source impactful, real-world data challenges.',
@@ -51,6 +54,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'teaching',
     name: 'UZH Department of Informatics',
     title: 'Head Teaching Assistant 🧑🏽‍🏫',
+    link: 'https://www.ifi.uzh.ch/en/seal/teaching/courses/info1.html',
     summaryPoints: [
       '📚 Served as Head Teaching Assistant for Informatics I (22AINF02) under the supervision of Prof. Dr. Harald Gall and Dr. Carol Alexandru, leading the teaching assistant team and managing teaching operations, exercise design, and assessment processes to ensure high-quality student learning support and effective course delivery.'
     ],
@@ -60,6 +64,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'research',
     name: 'Vrije Universiteit Amsterdam (VU Amsterdam) - Sports Intelligence Lab (SIL)',
     title: 'Research Assistant 👨🏽‍🔬',
+    link: 'https://sportsintelligencelab.com/',
     summaryPoints: [
       '🧪 Collaborating with Dr. Mauricio Verano Merino at the Sports Intelligence Lab (SIL) on multiple research projects, applying data-driven approaches in computer science and sports analytics.',
       'Conducted data-driven research on ball recovery in professional football, including exploration of machine learning applications in professional football.',
@@ -71,6 +76,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'research',
     name: 'Forward Football',
     title: 'AI Research Intern 👨🏽‍🔬',
+    link: 'https://forward.football/',
     summaryPoints: [
       '🧪 Analyzed large-scale spatiotemporal datasets to model player interactions, quantify team performance, and derive data-driven tactical insights.'
     ],
@@ -80,6 +86,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'teaching',
     name: 'Vrije Universiteit Amsterdam (VU Amsterdam)',
     title: 'Teaching Assistant Mentor 🧑🏽‍🏫',
+    link: 'https://vu.nl/en',
     summaryPoints: [
       '📚 Served as a mentor for new teaching assistants at VU Amsterdam, guiding them through the transition into instructional roles. Designed and delivered interactive workshops, developed training materials, and provided personalized feedback to support their growth in both confidence and teaching effectiveness.'
     ],
@@ -89,6 +96,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'volunteer',
     name: 'World Robot Olympiad (WRO) Netherlands',
     title: 'Judge at the National WRO competitions of Netherlands 🤖',
+    link: 'https://wro-association.org/',
     summaryPoints: [
       '🧑🏽‍⚖️ Served as a judge at the National World Robot Olympiad (WRO) competitions held in the Netherlands, where I evaluated student robotics projects and supported fair competition standards at the national level.'
     ],
@@ -98,6 +106,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'teaching',
     name: 'Vrije Universiteit Amsterdam (VU Amsterdam)',
     title: 'Head Teaching Assistant 🧑🏽‍🏫',
+    link: 'https://vu.nl/en',
     summaryPoints: [
       '📚 Served as the Head TA for the Computational Thinking course (X_400475) for BSc. Artificial Intelligence students, in collaboration with Dr. Aniel Bhulai. Oversaw and supported the course delivery for two academic years, each spanning approximately two months.'
     ],
@@ -107,6 +116,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'teaching',
     name: 'Vrije Universiteit Amsterdam (VU Amsterdam)',
     title: 'Teaching Assistant 🧑🏽‍🏫',
+    link: 'https://vu.nl/en',
     summaryPoints: [
       '📚 Served as a Teaching Assistant for multiple courses across the Faculty of Science and the Honors Program, primarily within the Computer Science and Artificial Intelligence departments.',
       'Assisted in Introduction to Python Programming (XB_0082) (2023, 2024).',
@@ -124,6 +134,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'work',
     name: 'Vrije Universiteit Amsterdam (VU Amsterdam)',
     title: 'Voting Member of the Program Committee 🤝🏽',
+    link: 'https://vu.nl/en',
     summaryPoints: [
       '⭐ Elected student representative and official voting member of the Program Committee for the BSc Artificial Intelligence program. Acted as the primary point of contact for AI students, collecting feedback and concerns regarding course content, teaching quality, and overall program structure.'
     ],
@@ -133,6 +144,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'work',
     name: 'Vrije Universiteit Amsterdam (VU Amsterdam)',
     title: 'International Student Ambassador 🌍',
+    link: 'https://vu.nl/en',
     summaryPoints: [
       '⭐ Selected by VU Amsterdam to represent the BSc. Artificial Intelligence program at key events and outreach initiatives. Acted as the face of the university during information sessions, student panels, and open days, as well as engaging with prospective students and their families, both locally and internationally.'
     ],
@@ -142,6 +154,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'work',
     name: 'Baridsoft | برید سامانه نوین',
     title: 'Information Technology Intern 💻',
+    link: 'https://baridsoft.net/',
     summaryPoints: [
       '👷🏽‍♂️ Completed a hands-on internship focused on computer systems, network infrastructure, and IT support. Gained practical experience in troubleshooting hardware and software issues, configuring network devices, and assisting in maintaining internal systems.'
     ],
@@ -151,6 +164,7 @@ const experienceTimeline: DisplayTimelineItem[] = [
     timelineType: 'work',
     name: 'University of Tehran',
     title: 'Captain of the Students Robotic Team 🤖',
+    link: 'https://me.ut.ac.ir/introduction',
     summaryPoints: [
       '🚀 Led the Student Robotics Team at the University of Tehran as a Team Captain, representing the university in the World Robot Olympiad (WRO) at both national and international levels. Oversaw project planning, robot design, programming, and team coordination.',
       'Ranked among the Top 16 teams worldwide at the 2017 WRO International Finals in Costa Rica.'
